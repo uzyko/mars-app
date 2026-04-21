52a1280b01ba650c1bb651d67eafcf38b2694d1b
uzyko <uzykoleksandr@gmail.com>
2026-04-21 22:21:59 +0300

    fix: add the missing import library

--- app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
+++ app/src/main/java/com/example/marsphotos/data/MarsPhotosRepository.kt
@@ -1,6 +1,7 @@
 package com.example.marsphotos.data
 
 import com.example.marsphotos.model.MarsPhoto
+import com.example.marsphotos.network.MarsApiService
 
 interface MarsPhotosRepository {
     suspend fun getMarsPhotos(): List<MarsPhoto>
