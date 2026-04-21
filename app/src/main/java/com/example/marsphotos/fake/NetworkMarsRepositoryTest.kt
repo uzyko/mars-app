c920f29b5826116ea26b6e034823588f38a62832
uzyko <uzykoleksandr@gmail.com>
2026-04-21 22:22:13 +0300

    test: write a repository test

---
+++ app/src/main/java/com/example/marsphotos/fake/NetworkMarsRepositoryTest.kt
@@ -0,0 +1,14 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.data.NetworkMarsPhotosRepository
+
+class NetworkMarsRepositoryTest {
+    @Test
+    fun networkMarsPhotosRepository_getMarsPhotos_verifyPhotoList() =
+        runTest {
+            val repository = NetworkMarsPhotosRepository(
+                marsApiService = FakeMarsApiService()
+            )
+            assertEquals(FakeDataSource.photosList, repository.getMarsPhotos())
+        }
+}
