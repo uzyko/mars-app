e44f27fc1eb689210b0ccb4901a40f2c8934c5fb
uzyko <uzykoleksandr@gmail.com>
2026-04-21 22:50:10 +0300

    fix: add the missing import library

--- app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
+++ app/src/main/java/com/example/marsphotos/network/MarsApiService.kt
@@ -17,6 +17,10 @@
 package com.example.marsphotos.network
 
 import com.example.marsphotos.model.MarsPhoto
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 import retrofit2.http.GET
 
     private const val BASE_URL =
